b3dc5df2f4d785ef3cce6bd0ead574c7e9324ffe
RobertoHB <robherblancalpe@gmail.com>
2019-06-14 16:11:52 +0200

    versionpoo

--- index.php
+++ index.php
@@ -1,21 +1,25 @@
 <?php
-
-
+Class Bombilla{
+	// vista
+	function on(){
+		return '
+			<a href="?off=1">
+				<img src="imagenes/bombilla_on.jpg" style="height:400px"/>
+			</a>
+		';
+	}
+	function off(){
+		return '
+			<a href="?on=1">
+				<img src="imagenes/bombilla_off.jpg" style="height:500px"/>
+			<a/>
+		';
+	}	
+}
 // controlador
-if( isset($_GET['on']) ) echo on();
-else echo off();
-function on(){
-	return '
-		<a href="?off=1">
-			<img src="imagenes/bombilla_on.jpg" style="height:400px"/>
-		</a>
-	';
-}
-function off(){
-	return '
-		<a href="?on=1">
-			<img src="imagenes/bombilla_off.jpg" style="height:500px"/>
-		<a/>
-	';
-}
+$b=new Bombilla();
+if( isset($_GET['on']) )
+	echo $b->on();
+else 
+	echo $b->off();
 ?>
